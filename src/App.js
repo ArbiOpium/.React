f542abdcc3c3f31ab856368539b4a12f4acb99c9
Yaroslav <bortnikyaroslav@gmail.com>
2025-06-05 06:20:20 +0300

    123

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 
+export function SideImage() {
+  return (
+    <><div className="side-image">
+      <div>
+      </div>
+      <img src="figma.jpg" className="side-image" alt="logo" />
+    </div><div className="login-right">
+        <form className="login-form">
+          <div className="form-group">
+            <label className="form-label">Електронна пошта або мобільний номер</label>
+            <input
+              type="text"
+              className="form-input"
+              placeholder="Email або номер" />
+          </div>
+
+          <div className="form-group">
+            <label className="form-label">Пароль</label>
+            <input type="password" className="form-input" placeholder="Ваш пароль" />
+          </div>
+
+          <button className="continue-button">ПРОДОВЖИТИ</button>
+        </form>
+      </div></>
+  );
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+        <SideImage />
+      </>
     </div>
   );
 }
